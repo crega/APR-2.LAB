d3e98dddd3679d1d650f373654ea6c52bde4f335
Vjuric <vj48819@fer.hr>
2018-11-03 15:47:22 +0100

    Add project files.

---
+++ APR 2.LAB/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APR_2.LAB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
